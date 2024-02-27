ff119fda4da18831fdab0b3ad4284e4c10452884
jjwalter368 <jjw368@gmail.com>
2024-02-27 15:23:07 -0500

    added an option for returning the output as a string or as json

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,30 @@
 use ureq;
+use serde_json::Value;
 
-pub fn postman_ureq(url: &str) -> String {
+
+pub fn postman_ureq_json(url: &str) -> Value {
+    let body: Value = serde_json::from_str(ureq::get(url).call().expect(".call failed").into_string().expect(".into_string failed").as_str()).expect("serde_json::from_str() failed");
+    body
+}
+pub fn postman_ureq_str(url: &str) -> String {
     let body: String = ureq::get(url).call().expect(".call failed").into_string().expect(".into_string failed");
     body
 }
+
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn apple_captive() {
-        let result = postman_ureq("https://captive.apple.com/");
+        let result = postman_ureq_str("https://captive.apple.com/");
         assert_eq!(result, "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>\n");
     }
     #[test]
     fn server_test() {
-        let result = postman_ureq("http://127.0.0.1:8000");
+        let result = postman_ureq_str("http://127.0.0.1:8000");
         assert_eq!(result, "Hello, world!");
 
     }
